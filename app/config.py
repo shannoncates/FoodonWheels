8c9b291b588666ea45877d5142bb2078f74a98c8
shannoncates <xan19@ymail.com>
2016-12-17 17:38:00 +0800

    Added Config file

---
+++ app/config.py
@@ -0,0 +1,17 @@
+DEBUD = True
+
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+SQLALCHEMY_TRACK_MODIFICATIONS = True
+SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://postgres:monkeyka19@localhost/DeliverFood"
+SQLALCHEMY_MIGRATION_REPO = os.path.join(basedir, 'db_repository')
+
+
+# Use a secure, unique and absolutely secret key for
+# signing the data
+CSRF_SESSION_KEY = "secret"
+
+
+# Secret key for signing cookies
+SECRET_KEY = "secret"
