45e2c85e285f41303e4576066cbf795a7d730cc6
shannoncates <xan19@ymail.com>
2016-12-19 23:27:31 +0800

    Modified database

---
+++ app/database_setup.py
@@ -0,0 +1,75 @@
+import os
+import sys
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key = True)
+    name = Column(String(80), nullable = False)
+    email = Column(String(50), nullable = False)
+
+class Restaurant(Base):
+    __tablename__ = 'restaurant'
+
+    id = Column(Integer, primary_key = True)
+    name = Column(String(80), nullable = False)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+    menuItems = relationship("MenuItem", cascade="all, delete-orphan")
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+            'name': self.name,
+            'id': self.id,
+        }
+
+
+class MenuItem(Base):
+    __tablename__ = 'menu_item'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(80), nullable=False)
+    course = Column(String(250))
+    description = Column(String(250))
+    price = Column(String(8))
+    restaurant_id = Column(Integer, ForeignKey('restaurant.id'))
+    restaurant = relationship(Restaurant)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+
+
+    @property
+    def serialize(self):
+        return {
+            'name': self.name,
+            'description': self.description,
+            'id': self.id,
+            'price': self.price,
+            'course': self.course,
+        }
+
+
+
+
+engine = create_engine('postgres://zzzqbabcxjnsmv:3he9x8m6CkOqpuoZUCULYrFAxr@ec2-54-204-15-48.compute-1.amazonaws.com:5432/dbg7psqitc22e5')
+'''
+from sqlalchemy import MetaData
+meta = MetaData()
+import contextlib
+with contextlib.closing(engine.connect()) as con:
+    trans = con.begin()
+    for table in reversed(meta.sorted_tables):
+        con.execute(table.delete())
+    trans.commit()
+'''
+
+Base.metadata.create_all(engine)
