e86883dbd0479706f3d2644038736ec1f9d85e01
Chezl <ezlvansa_myxc002@yahoo.com>
2016-12-19 15:05:03 +0800

    modified features

--- app/forms.py
+++ app/forms.py
@@ -1,4 +1,4 @@
-from flask_wtf import Form
+#from flask_wtf import Form
 from wtforms import StringField, BooleanField, TextAreaField, Form, TextField, PasswordField, validators
 from wtforms.validators import DataRequired, Length
 
