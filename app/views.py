509bce634253887a62d2758e444c941a37a1572b
Chezl <ezlvansa_myxc002@yahoo.com>
2016-12-19 15:02:08 +0800

    added transaction and orders

--- app/views.py
+++ app/views.py
@@ -1,16 +1,16 @@
-from flask_login import LoginManager, UserMixin, login_required, login_user, logout_user
+#from flask_login import LoginManager, UserMixin, login_required, login_user, logout_user
 from flask import Flask, render_template, redirect, url_for, request, Response, flash, jsonify, send_file, send_from_directory, g
 from flask_sqlalchemy import SQLAlchemy
-from flask_httpauth import HTTPBasicAuth, HTTPTokenAuth
+#from flask_httpauth import HTTPBasicAuth, HTTPTokenAuth
 from sqlalchemy.orm import sessionmaker
 from werkzeug.exceptions import abort
-from passlib.hash import sha256_crypt
+#from passlib.hash import sha256_crypt
 from sqlalchemy import create_engine
 from models import DBconn, User
 from sqlalchemy.orm import exc
 from datetime import datetime
 from app import app, db
-from forms import *
+#from forms import *
 import hashlib
 import sys, os
 
@@ -61,13 +61,12 @@ def authentication():
         return jsonify({'status': status, 'message': 'error'})
     else:
         status = True
-return jsonify({'status': status, 'message': 'Successfully Logged In'})
+	return jsonify({'status': status, 'message': 'Successfully Logged In'})
 
-@auth.login_required
 @app.route('/api/logout')
 def logout():
     """ User logout function """
-return jsonify({'status':'ok','message':'logged out'})
+    return jsonify({'status':'ok','message':'logged out'})
 
 
 #######################################################
@@ -199,4 +198,138 @@ def search():
 		if len(recs) == 0:
 			return jsonify({'status': 'no entries', 'count': len(recs)})
 		return jsonify({'status': 'ok', 'entries': recs, 'count': len(recs)})
+####################################################################################
+
+############################## ORDER ######################################
+
+
+@auth.login_required
+@app.route('/api/order', methods=['GET'])
+def get_all_order():
+    """ Retrieves all private messages in the database """
+
+    res = spcall('list_orders', ())
+    if 'Error' in str(res[0][0]):
+        return jsonify({'status': 'error', 'message': res[0][0]})
+
+    recs = []
+    for r in res:
+        recs.append({'order id': r[0], 'user id': r[1], 'food id': 'is_active': r[2]})
+return jsonify({'status': 'ok', 'entries': recs, 'count': len(recs)})
+
+@auth.login_required
+@app.route('/api/order/<>', methods=['POST'])
+def add_transactions():
+    """ Add a new private message using a stored procedure """
+    
+    mess = spcall('list_orders_database', ())
+    orderid = len(mess) +1
+    
+    res = spcall('add_order', (orderid, usesrid, foodid, ), True)
+    if 'Error' in str(res[0][0]):
+        return jsonify({'status': 'error', 'message': res[0][0]})
+         
+return jsonify({'status': 'ok', 'message': ''})
+
+
+@auth.login_required
+@app.route('/api/transaction/<int:order_id>', methods=['PUT'])
+def delete_order(order_id):
+	""" Delete a feedback """
+	
+	# List all feedback id
+	order_list = spcall('list_orders', ())
+	active_order_ids = []
+	for order in order_list:
+		active_order_ids.append(order[0])  
+
+	valid_order_id = False
+
+	# Validate feedback id
+	for entry in active_order_ids:
+		if (entry == order_id):
+			valid_order_id = True
+
+	if (valid_order_id):
+		res = spcall('delete_order', str(order_id), True)
+		return jsonify({'status': 'successful', 'message': 'order successfully removed'})
+
+return jsonify({'status': 'error', 'message': 'an error was encountered'})
+
+
+
+####################################################################################
+
+
+
+########################### TRANSACTION #################################
+
+@auth.login_required
+@app.route('/api/transaction', methods=['GET'])
+def get_all_transaction():
+    """ Retrieves all private messages in the database """
+
+    res = spcall('list_transactions', ())
+    if 'Error' in str(res[0][0]):
+        return jsonify({'status': 'error', 'message': res[0][0]})
+
+    recs = []
+    for r in res:
+        recs.append({'transaction id': r[0], 'order id': r[1], 'total bill': r[2], 'datetransact': r[3], 'is_active': r[4]})
+return jsonify({'status': 'ok', 'entries': recs, 'count': len(recs)})
+
+@auth.login_required
+@app.route('/api/transaction/<int:order_id>', methods=['GET'])
+def get_transaction(receiverid):
+    """ Retrieves all private messages from a receiver who matches the receiver id parameter """
+
+    res = spcall('show_transactions', [str(orderid)])
+        
+
+    recs = []
+    for r in res:
+        recs.append({'transaction id': r[0], 'order id': r[1], 'total bill': r[2], 'datetransact': r[3], 'is_active': r[4]})
+    if len(recs) == 0:
+        return jsonify({'status': 'error', 'message': "message not found"})
+return jsonify({'status': 'ok', 'entries': recs, 'count': len(recs)})
+
+
+@auth.login_required
+@app.route('/api/transaction/<>', methods=['POST'])
+def add_transactions():
+    """ Add a new private message using a stored procedure """
+    
+    mess = spcall('list_transactions_database', ())
+    transactionid = len(mess) +1
+    
+    res = spcall('add_transactions', (transactionid, orderid, totalbill, deliver, foodquantity, foodname), True)
+    if 'Error' in str(res[0][0]):
+        return jsonify({'status': 'error', 'message': res[0][0]})
+         
+return jsonify({'status': 'ok', 'message': ''})
+
+
+@auth.login_required
+@app.route('/api/transaction/<int:order_id>', methods=['PUT'])
+def delete_transaction(feedback_id):
+	""" Delete a feedback """
+	
+	# List all feedback id
+	transaction_list = spcall('list_transactions', ())
+	active_transaction_ids = []
+	for transaction in transaction_list:
+		active_transaction_ids.append(transaction[0])  
+
+	valid_transaction_id = False
+
+	# Validate feedback id
+	for entry in active_transaction_ids:
+		if (entry == transaction_id):
+			valid_transaction_id = True
+
+	if (valid_transaction_id):
+		res = spcall('delete_transactions', str(transaction_id), True)
+		return jsonify({'status': 'successful', 'message': 'transaction successfully removed'})
+
+return jsonify({'status': 'error', 'message': 'an error was encountered'})
 ####################################################################################
