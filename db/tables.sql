cd965b98c4bffc45808d9cd439725734ecb5df59
Chezl <ezlvansa_myxc002@yahoo.com>
2017-12-09 23:56:06 +0800

    modified sql

--- db/tables.sql
+++ db/tables.sql
@@ -1,14 +1,47 @@
 
 create table "User"
 (
-  first_name text primary key,
-  last_name text,
-  middle_initial text,
+  user_id int primary key,
+  fname text,
+  lname text,
   email text unique,
+  user_location text,
+  user_contact int,
   password text,
+  awardpoints real default 0, 
   is_authenticated boolean default True,
   is_active boolean default True,
   is_anonymous boolean default True,
-  role text
+);
+
+create table "Restaurant"
+(
+   restaurant_id primary key,
+   restaurant_name text,
+   is_active boolean default True
+);
+
+create table "Food"
+(
+   food_id primary key,
+   food_name text,
+   is_active boolean default True
+)
+
+create table "Categories"
+(
+  category_id int primary key,
+  category_name text,
+  is_active boolean default True
+);
+
+create table "Feedback"
+(
+  feedback_id int primary key,
+  restaurant_id int references "Restaurant",
+  food_id text references "Food",
+  body text,
+  date_published timestamp,
+  is_active boolean default True
 );
 
