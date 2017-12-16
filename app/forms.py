6442ddee0e05cbcd8f95213e134244083226f4f1
Chezl <ezlvansa_myxc002@yahoo.com>
2017-12-16 08:06:04 +0800

    modified forms.py

--- app/forms.py
+++ app/forms.py
@@ -2,12 +2,15 @@ from flask.ext.wtf import Form
 from wtforms import StringField, BooleanField, TextAreaField, Form, TextField, PasswordField, validators
 from wtforms.validators import DataRequired, Length
 
-class RegisterForm(Form):
-	username = TextField('Username', [validators.Length(min=4, max=25)])
-	email = TextField('Email Address', [validators.Length(min=6, max=35)])
-	password = PasswordField('New Pssword', [validators.Required()])
-	accept_tos = BooleanField('I accept the TOS', [validators.Required()])
 
-class LoginForm():
+class RegisterForm(Form):
+		username = TextField('Username', [validators.Length(min=4, max=25)])
+		email = TextField('Email Address', [validators.Length(min=6, max=35)])
+		password = PasswordField('New Password', [validators.Required(), validators.EqualTo('confirm', message='Passwords must match')])
+		confirm = PasswordField('Repeat Password')
+		accept_tos = BooleanField('I accept the TOS', [validators.Required()])
+
+
+class LoginForm(Form):
 	username = TextField('Email', [validators.Length(min=4, max=25)])
 	password = PasswordField('Password', [validators.Required()])
