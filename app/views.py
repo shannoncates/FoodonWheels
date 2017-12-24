3acc7e35f3ec01f15ec234f7ed52db5ee18a25b3
Chezl <ezlvansa_myxc002@yahoo.com>
2017-12-24 09:39:45 +0800

    Modified views.py

--- app/views.py
+++ app/views.py
@@ -1,9 +1,18 @@
 from flask import Flask, send_file, render_template, send_from_directory, jsonify
+from flask_httpauth import HTTPBasicAuth
 from app import app
+from models import DBconn
+from sqlalchemy import create_engine
+from flask import url_for
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import Form
+import hashlib
 import sys, os
 
-APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+key = "1290gath43quz1@"
+auth = HTTPBasicAuth()
 
 def spcall(qry, param, commit=False):
     """ Function which communicates with the database """
@@ -29,6 +38,15 @@ def home():
 
     return send_from_directory(os.path.join(APP_ROOT, 'static', 'html'), 'index.html')
 
+
+#def verify_token(token):
+
+#    recs = spcall('list_users', ())
+
+#    for element in recs:
+#        if token == hashlib.md5( str(element[4] + element[7] ). encode()).hexdigest(): #email + password 
+#            return True
+#        return False
     
 
 @app.errorhandler(404)
@@ -53,9 +71,17 @@ def index():
 @app.route('/api/registeruser', methods=['POST'])
 def register():
 
-	passw = request.form["password"]
-	passw = passw + key
-	passw = hashlib.md5(passw.encode())
+    user_id = len(spcall("list_users", ())) + 1
+    fname = request.form["fname"]
+    lname = request.form["lname"]
+    minitial = request.form["minitial"]
+    email = request.form["email"]
+    user_location = request.form["user_location"]
+    user_contact = request.form["user_contact"]
+	password = request.form["password"]
+	password = password + key
+	password = hashlib.md5(password.encode())
+    password = password.hexdigest()
 	email = request.form["email"]
 
 	if email == '':
@@ -81,22 +107,24 @@ def register():
 	if 'Error' in str(recs[0][0]):
 		return jsonify({'status': 'error', 'message': recs[0][0]})
 	for element in recs:
-		if email == str(element[0]):
-			return jsonify({'status': 'error2', 'message': 'email already exists'})
+		if email == str(element[4]):
+			return jsonify({'status': 'error', 'message': 'email already exists'})
 
-	res = spcall('add_user', (
-		request.form["email"],
-		request.form["firstname"],
-		request.form["middlei"],
-		request.form["lastname"],
-		request.form["contactno"],
-		request.form["address"],
-		passw
-		), True)
+    res = spcall("add_user", (user_id, fname, lname, minitial, email, user_location, user_contact, password), True)
+    return jsonify({"status: "200", "message": "success"})
+	#res = spcall('add_user', (
+	#	request.form["email"],
+	#	request.form["firstname"],
+	#	request.form["middlei"],
+	#	request.form["lastname"],
+	#	request.form["contactno"],
+	#	request.form["address"],
+	#	passw
+	#	), True)
 
-	if 'Error' in str(res[0][0]):
-		return jsonify({'status': 'error', 'message': res[0][0]})
-	return jsonify({'status': 'ok', 'message': res[0][0]})
+	#if 'Error' in str(res[0][0]):
+	#	return jsonify({'status': 'error', 'message': res[0][0]})
+	#return jsonify({'status': 'ok', 'message': res[0][0]})
 
 @app.route('/api/loginuser', methods=['POST'])
 def login():
